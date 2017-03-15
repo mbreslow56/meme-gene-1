511fbec441710b158b11620039a131777be72389
mbreslow56 <mbreslow56@gmail.com>
2017-03-15 13:30:48 +0200

    can add text and watermark to white box

--- public/js/controllers/textCtrl.js
+++ public/js/controllers/textCtrl.js
@@ -2,4 +2,8 @@ app.controller('textCtrl', function($scope) {
 	$scope.text = "";
 });
 
+app.controller('textSizeCtrl', function($scope){
+	$scope.size = 28;
+})
+
 //angular.module('app', ['nmv.angular']).controller('TestCtrl', ["canvasFitText", function (canvasFitText) {}]);
--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-
-
-ctx.beginPath();
-ctx.rect(20, 20, 320, 320);
-ctx.fillStyle = "white";
-ctx.fill();
