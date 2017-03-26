6c60edbd540e60543bea3a53cab4a2eafbd48f95
mbreslow56 <mbreslow56@gmail.com>
2017-03-26 17:29:44 +0300

    supposedly done

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ app.use(function(err, req, res, next) {
 
 //start the server
 app.listen('27017', function() {
-  console.log("yo yo yo, on 8000 bro");
+  console.log("yo yo yo, on 27017 bro");
 });
